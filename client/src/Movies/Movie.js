da5987b84fd848f6b8fd79abfaa6c56b7e551e7d
natecheney-dev <natecheney@gmail.com>
2021-09-13 15:59:33 -0500

    finished project

--- client/src/Movies/Movie.js
+++ client/src/Movies/Movie.js
@@ -1,26 +1,33 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import { useParams } from 'react-router-dom';
 
 export default function Movie(props) {
   const [movie, setMovie] = useState();
+  // console.log(props);
 
-  let id = 1;
+  const {id} = useParams();
+  const itemID = props.item.find(stuff => stuff.id == (id))
+
+  console.log(itemID);
   // Change ^^^ that line and use a hook to obtain the :id parameter from the URL
-
+  
   useEffect(() => {
     axios
       .get(`http://localhost:5000/api/movies/${id}`) // Study this endpoint with Postman
       .then(response => {
         // Study this response with a breakpoint or log statements
         // and set the response data as the 'movie' slice of state
+        setMovie(response.data);
       })
       .catch(error => {
         console.error(error);
       });
     // This effect should run every time time
     // the `id` changes... How could we do this?
-  }, []);
+  }, [id]);
 
+  // console.log(movie);
   // Uncomment this only when you have moved on to the stretch goals
   // const saveMovie = evt => { }
 
@@ -29,6 +36,7 @@ export default function Movie(props) {
   }
 
   const { title, director, metascore, stars } = movie;
+  
 
   return (
     <div className="save-wrapper">
@@ -47,6 +55,7 @@ export default function Movie(props) {
             {star}
           </div>
         ))}
+
       </div>
       <div className="save-button">Save</div>
     </div>
--- client/src/Movies/MovieList.js
+++ client/src/Movies/MovieList.js
@@ -1,27 +1,40 @@
 import React from 'react';
-
-export default function MovieList(props) {
+import { useHistory, Link } from 'react-router-dom';
+export default function MovieList(props) { 
   return (
     <div className="movie-list">
       {props.movies.map(movie => (
         <MovieDetails key={movie.id} movie={movie} />
       ))}
     </div>
-  );
+    );
 }
 
+
+
+
 function MovieDetails(props) {
-  const { title, director, metascore } = props.movie;
+  const { title, director, metascore, id } = props.movie;
+
+  const history = useHistory();
+
+  const routeToMovie = () => {
+    history.push(`/save-wrapper/${id}`);
+  }
+
 
   return (
-    <div className="movie-card">
+    <div onClick={routeToMovie} className="movie-card">
+  
       <h2>{title}</h2>
+    
       <div className="movie-director">
         Director: <em>{director}</em>
       </div>
       <div className="movie-metascore">
         Metascore: <strong>{metascore}</strong>
       </div>
+      
     </div>
   );
 }
